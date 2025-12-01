513ba24fc7aa1a44bbe574facc86e074512c9113
charithram301 <charithrambhimesetty@gmail.com>
2025-12-01 22:51:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 
-data_path = r"C:\Users\chari\OneDrive\P587 DATASET.csv"
+data_path = r"P587 DATASET.csv"
 
 st.set_page_config(page_title="SARIMA Quick Deploy", layout="wide")
 
@@ -73,6 +73,7 @@ if st.button("Train SARIMA"):
        
 
         
+
 
 
 
